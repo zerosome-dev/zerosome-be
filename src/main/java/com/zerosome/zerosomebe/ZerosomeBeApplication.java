97d97933b9a4a8501b5656517a03ff0102e1049a
akalswl14 <gnkryd12@gmail.com>
2024-05-18 16:58:06 +0900

    setting: Init project

---
+++ src/main/java/com/zerosome/zerosomebe/ZerosomeBeApplication.java
@@ -0,0 +1,13 @@
+package com.zerosome.zerosomebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerosomeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerosomeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerosome/zerosomebe/ZerosomeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerosome.zerosomebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerosomeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
