6e1a75d215819a0bbddc11caf7e89eb8070fd0ba
akalswl14 <gnkryd12@gmail.com>
2024-08-31 14:28:23 +0900

    카테고리 이미지 경로 수정

--- src/main/java/com/zerosome/zerosomebe/domain/category/repository/CategoryJpaRepository.java
+++ src/main/java/com/zerosome/zerosomebe/domain/category/repository/CategoryJpaRepository.java
@@ -10,7 +10,7 @@ import com.zerosome.zerosomebe.domain.code.entity.CommCode;
 
 public interface CategoryJpaRepository extends JpaRepository<CommCode, Long> {
     @Query("select new com.zerosome.zerosomebe.domain.category.dto.AllFoodCategory("
-           + "d1.code, d1.codeDesc, d2.code, d2.codeDesc, concat('category/', d2.code,'.png'), d2.subNoOptionYn) "
+           + "d1.code, d1.codeDesc, d2.code, d2.codeDesc, concat('prod/category/', d2.code,'.png'), d2.subNoOptionYn) "
            + "from COMM_CODE d1 "
            + "left join COMM_CODE d2 on d1.code = d2.superCode "
            + "where d1.groupCode = 'CTG' and d1.superCode is null "
